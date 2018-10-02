e0f0463bebcfe134ee46dbbe6bf34b95d72ba355
Blake <bsward8@gmail.com>
2018-10-02 21:10:38 +1000

    refactor: major refinements and polish, embed for board

--- src/commands/gameCommands/tictactoe.js
+++ src/commands/gameCommands/tictactoe.js
@@ -30,25 +30,24 @@ module.exports = new GenericCommand(
       let prompt = await Memer.MessageCollector.awaitMessage(msg.channel.id, player.id, 30e3);
       if (!prompt) {
         msg.channel.createMessage(`${player.username} didn't answer in time, what a noob. ${opponent.username} wins`);
-      }
-      let markers = prompt.content.toLowerCase().split('');
-      let row = markers[0];
-      let column = Number(markers[1]);
-
-      if (row === 'a') {
-        board[0].splice(column - 1, 1, player);
-      } else if (row === 'b') {
-        board[1].splice(column - 1, 1, player);
-      } else if (row === 'c') {
-        board[2].splice(column - 1, 1, player);
-      }
-
-      if (prompt.content.toLowerCase() === 'end') {
-        msg.channel.createMessage(`**${player.username}** has ended the game what a wimp`);
       } else {
-        msg.channel.createMessage(`**${player.username}**, that's not a valid option lmao! You must type \`punch\`, \`defend\` or \`end\` in chat!\n${retry ? 'The game has ended due to multiple invalid choices, god ur dumb' : ''}`);
-        if (!retry) {
-          return performTurn(player, opponent, true);
+        let markers = prompt.content.toLowerCase().split('');
+        let row = markers[0];
+        let column = Number(markers[1]);
+
+        if (row === 'a' && column) {
+          return board[0].splice(column - 1, 1, player);
+        } else if (row === 'b' && column) {
+          return board[1].splice(column - 1, 1, player);
+        } else if (row === 'c' && column) {
+          return board[2].splice(column - 1, 1, player);
+        } else if (prompt.content.toLowerCase() === 'end') {
+          msg.channel.createMessage(`**${player.username}** has ended the game what a wimp`);
+        } else {
+          msg.channel.createMessage(`**${player.username}**, that's not a valid option lmao! Type out \`a\`, \`b\` or \`c\` for the row, then \`1\`, \`2\` or \`3\` for the column. (eg. \`a1\` for top-left or \`b2\` for middle)\n${retry ? 'The game has ended due to multiple invalid choices, god ur dumb' : ''}`);
+          if (!retry) {
+            return performTurn(player, opponent, true);
+          }
         }
       }
     };
@@ -59,17 +58,14 @@ module.exports = new GenericCommand(
         if (board[i][0] !== null && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
           return true;
         }
-
         // vertical
         if (board[0][i] !== null && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
           return true;
         }
-
         // downward diagonal
         if (board[0][0] !== null && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
           return true;
         }
-
         // upward diagonal
         if (board[2][0] !== null && board[2][0] === board[1][1] && board[1][1] === board[0][2]) {
           return true;
@@ -80,31 +76,35 @@ module.exports = new GenericCommand(
 
     const layoutBoard = (space) => {
       if (!space) {
-        return ' - ';
+        return ' *•* ';
       } else {
         if (space === author) {
-          return ' X ';
+          return ' **X** ';
         } else {
-          return ' O ';
+          return ' **O** ';
         }
       }
     };
 
     const play = async () => {
-      const damage = await performTurn(turn, oppturn);
-      if (damage === undefined) {
+      const move = await performTurn(turn, oppturn);
+      if (move === undefined) {
         return;
       }
-      if (!damage) {
+      if (!move) {
         oppturn = [turn, turn = oppturn][0];
         return play();
       }
-      msg.channel.createMessage(`${board.top.map(layoutBoard())}\n${board.middle.map(layoutBoard())}\n${board.bottom.map(layoutBoard())}`);
+      msg.channel.createMessage({
+        title: `Tic Tac Toe - ${oppturn.username} vs ${turn.username}`,
+        description: `${board[0].map(layoutBoard)}\n${board[1].map(layoutBoard)}\n${board[2].map(layoutBoard)}`,
+        footer: { text: `It is currently ${turn.username}'s turn` }
+      });
       if (!checkMatch()) {
         oppturn = [turn, turn = oppturn][0];
         return play();
       } else {
-        return `congrats like ${turn.username} won bitches!!!`;
+        return `congrats ${turn.username} won!!!`;
       }
     };
     play();

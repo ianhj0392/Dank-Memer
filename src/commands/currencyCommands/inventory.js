c6a31a0154cd44b63cda3d5d86ee6a9a2fc7ca97
Blake <bsward8@gmail.com>
2018-10-07 07:59:46 +1100

    feat: inventory command

---
+++ src/commands/currencyCommands/inventory.js
@@ -0,0 +1,34 @@
+const GenericCurrencyCommand = require('../../models/GenericCurrencyCommand');
+
+module.exports = new GenericCurrencyCommand(
+  async ({ Memer, msg, args, addCD, userEntry }) => {
+    const page = Number(msg.args.nextArgument) || 1;
+    const user = msg.args.resolveUser();
+    const userItems = userEntry.props.inventory;
+    const items = [];
+    for (let i of userItems) {
+      items.push(`**${i.name}** ─ *ID* \`${i.id}\` ─ ${i.type.charAt(0).toUpperCase()}${i.type.slice(1)}\n`);
+    }
+
+    if (items < 1) {
+      return 'You suck, you have nothing. You can use `pls shop` to see what\'s in store though';
+    }
+    Memer.paginationMenu(items, {
+      type: 'Owned Items',
+      embed: {
+        author:
+          {
+            name: `${user.username}#${user.discriminator}`,
+            icon_url: user.dynamicAvatarURL()
+          }
+      },
+      pageLength: 7
+    }, page);
+  },
+  {
+    triggers: ['steal', 'rob', 'ripoff'],
+    cooldown: 5e3,
+    donorCD: 3e3,
+    perms: ['embedLinks']
+  }
+);

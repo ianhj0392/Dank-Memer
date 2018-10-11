d93fde49004d0408d95dd992e2a965fce2f6059f
Blake <bsward8@gmail.com>
2018-10-11 23:49:31 +1100

    feat: move currency related items to separate JS file

---
+++ src/models/Currency.js
@@ -0,0 +1,110 @@
+/**
+ * Levels
+*/
+/** Rewards is always the same object, but can contain of 4 things:
+  * coins
+  * items (array of objects, laid out like {id: item ID, quantity: number})
+  * multiplier (number)
+  * title (string)
+*/
+const LEVELS = {
+  1: { reward: { coins: 500 }, exp: 100 },
+  3: { reward: { coins: 1500 }, exp: 300 },
+  5: { reward: { multiplier: 2.5 }, exp: 500 },
+  7: { reward: { coins: 4e3, items: [{ tidepod: 2 }] }, exp: 700 },
+  9: { reward: { coins: 4e3, items: [{ spinner: 3 }] }, exp: 900 },
+  10: { reward: { multiplier: 5 }, exp: 1000 },
+  11: { reward: { items: [{ reversal: 1 }] }, exp: 1100 },
+  13: { reward: { coins: 5e3, items: [{ inviscloak: 3 }] }, exp: 1300 },
+  15: { reward: { multiplier: 5 }, exp: 1500 },
+  17: { reward: { coins: 5e3 }, exp: 1700 },
+  19: { reward: { coins: 7e3 }, exp: 1900 },
+  20: { reward: { coins: 1e4 }, exp: 2000 },
+  21: { reward: { coins: 500 }, exp: 2100 },
+  23: { reward: { coins: 500 }, exp: 2300 },
+  25: { reward: { multiplier: 7.5 }, exp: 2500 }
+};
+
+/**
+ * Items
+*/
+const ItemTypes = {
+  ITEM: 'Item',
+  COLLECTABLE: 'Collectable',
+  TOOL: 'Tool',
+  POWERUP: 'Power-up'
+};
+
+const SHOP = {
+  tidepod: {
+    name: 'Tidepod',
+    id: 'tidepod',
+    type: ItemTypes.ITEM,
+    cost: 15e3,
+    consumable: true
+  },
+  inviscloak: {
+    name: 'Invisibility Cloak',
+    id: 'inviscloak',
+    type: ItemTypes.POWERUP,
+    cost: 35e3,
+    consumable: true
+  },
+  reversal: {
+    name: 'Reversal Card',
+    id: 'reversal',
+    type: ItemTypes.POWERUP,
+    cost: 20e3,
+    consumable: true
+  },
+  spinner: {
+    name: 'Fidget Spinner',
+    id: 'reversal',
+    type: ItemTypes.POWERUP,
+    cost: 20e3,
+    consumable: true
+  },
+  pepe: {
+    name: 'Rare Pepe',
+    id: 'pepe',
+    type: ItemTypes.COLLECTABLE,
+    cost: 1e5,
+    consumable: false
+  }
+};
+
+/**
+ * Loot Boxes
+*/
+// The amount of arrays in `items` represents getting multiple items of different tiers (ie, one amazing item, multiple medium items)
+// It's important to note that people can sell items meaning they can gain back value on items gained in boxes
+// Descriptions need better wording, not sure if I'll keep them
+const BOXES = {
+  NORMIE: { // 50 to 500 coins, 20% chance to get a low item
+    level: 1,
+    name: 'Normie Box',
+    description: 'Can\'t get more basic than this',
+    rewards: {
+      coins: { min: 50, max: 500 },
+      items: [{ sand: 1 }, { reversal: 1 }]
+    }
+  },
+  MEME: { // 1000 to 3000 coins, 75% chance to get a medium item
+    level: 2,
+    description: 'Something actually worth opening',
+    rewards: {
+      coins: { min: 1e3, max: 3e3 },
+      items: [{ phone: 1 }, { tidepod: 1 }]
+    }
+  },
+  DANK: { // 7500 to 10000 coins, 100% chance to get one amazing item, multiple medium items
+    level: 3,
+    description: 'Dank rewards for a dank donator',
+    rewards: {
+      coins: { min: 7.5e3, max: 1e4 },
+      items: [[{ inviscloak: 1 }, { reversal: 1 }], [{ tidepod: 3 }, { alcohol: 1 }, { phone: 2 }]]
+    }
+  }
+};
+
+module.exports = { shop: SHOP, levels: LEVELS, boxes: BOXES };

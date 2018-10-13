69d2ee7f466c720c311af5235e2b584ddbe78665
Blake <bsward8@gmail.com>
2018-10-14 01:01:35 +1100

    fix: return message on consume and other fixes

--- src/commands/currencyCommands/use/index.js
+++ src/commands/currencyCommands/use/index.js
@@ -13,24 +13,28 @@ module.exports = new GenericCurrencyCommand(
     if (!item.consumable) {
       return 'You can\'t use this item :thinking:';
     }
+    if (await userEntry.getActiveItems(item.id)) {
+      return 'You\'ve already used this item!';
+    }
     if (item.type === Currency.ItemTypes.BOX) {
       const emoji = item.id === 'normie' ? ':wastebasket:' : item.id === 'meme' ? ':stars:' : ':trident:';
       msg.channel.createMessage(`${emoji} Opening a ${item.name}... ${emoji}`);
       await Memer.sleep(2500);
 
       const coins = Math.floor(Memer.randomNumber(item.rewards.coins.min, item.rewards.coins.max));
-      const reward = Object.keys(Memer.randomInArray(item.rewards.items))[0];
+      const [itemid, amount] = Object.keys(Memer.randomInArray(item.rewards.items))[0];
       userEntry.addPocket(coins);
-      userEntry.addInventoryItem(reward[0], reward[1]);
+      userEntry.addInventoryItem(itemid, amount);
 
       await userEntry.removeInventoryItem(item.id).save();
-      return `you got ${coins} coins wooow`;
+      return `good stuff, you got a solid **${coins}** coins and \`${amount} ${Currency.items[itemid].name}${amount !== 1 ? '\'s' : ''}\`!`;
     }
 
     const consume = items[item.id].fn({ Memer, msg, userEntry, donor, Currency, isGlobalPremiumGuild });
     if (consume) {
       await userEntry.removeInventoryItem(item.id).save();
     }
+    return consume;
   }, {
     triggers: ['use', 'consume'],
     usage: '{command} [item]',

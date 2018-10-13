b37cf609a7919ed18eb9cd7d3fff15189a8eafdc
Blake <bsward8@gmail.com>
2018-10-13 23:48:16 +1100

    fix: add messages to 'sand' item and failure rate

--- src/commands/currencyCommands/use/items/sand.js
+++ src/commands/currencyCommands/use/items/sand.js
@@ -1,16 +1,18 @@
 module.exports = {
   fn: async ({ Memer, msg, Currency, userEntry }) => {
     const random = Memer.randomNumber();
+    const user = msg.args.resolveUser();
     if (random < 30) {
       Memer.redis.set(`sand-effect-${msg.author.id}`, JSON.stringify({ perpetrator: msg.author.id }), 'EX', 60 * 60);
+      return `The wind wasn't blowing in your favor and you ended up getting sand in your OWN eyes, good going.\nYou can't steal from ${user.username} for another 6 hours.`;
     } else {
-      const user = msg.args.resolveUser();
       const sand = await Memer.redis.get(`sand-effect-${user.id}`)
         .then(res => res ? JSON.parse(res) : undefined);
       if (sand) {
         return 'This person already has sand in their eyes, no need to bully them further';
       }
       Memer.redis.set(`sand-effect-${user.id}`, JSON.stringify({ perpetrator: msg.author.id }), 'EX', 60 * 60);
+      return `You pull out a bit of old, dirty sand from your pocket and blow it right into ${user.username}'s face.\n${user.username} has been blinded and they can't steal from you for another 6 hours.`;
     }
   }
 };

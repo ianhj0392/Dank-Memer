6637f9129b11b553c7271ae1d59c879f00cff5f0
Blake <bsward8@gmail.com>
2018-10-14 00:25:38 +1100

    feat: implement 'tidepod' item functionality

--- src/commands/currencyCommands/use/items/tidepod.js
+++ src/commands/currencyCommands/use/items/tidepod.js
@@ -1,11 +1,13 @@
 module.exports = {
-  fn: async ({ Memer, Currency, userEntry }) => {
-    const random = Memer.random(0, 1);
+  fn: async ({ Memer, msg, Currency, userEntry }) => {
+    const random = Math.random();
 
     if (random > 0.2) { // no die
-      // multiplier
+      Memer.redis.set(`activeitems-${msg.author.id}-tidepod`, 5, 'EX', 60 * 60 * 24);
+      return 'You eat a tidepod and somehow live to see the benefits. You gained a **5% multiplier** for **24 hours**!';
     } else { // die
       await userEntry.removePocket(userEntry.props.pocket).save();
+      return `Although it got you a lot of internet points and karma, you ended up dying from the tidepod you ate.\nYou lost **all of your coins**.`;
     }
   }
 };

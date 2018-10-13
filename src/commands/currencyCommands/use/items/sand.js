e1d7177e53163bd126fc3ded361826c67dc2ea4d
Blake <bsward8@gmail.com>
2018-10-14 00:19:27 +1100

    fix: that's not 6 hours blake

--- src/commands/currencyCommands/use/items/sand.js
+++ src/commands/currencyCommands/use/items/sand.js
@@ -3,7 +3,7 @@ module.exports = {
     const random = Memer.randomNumber();
     const user = msg.args.resolveUser();
     if (random < 30) {
-      Memer.redis.set(`sand-effect-${msg.author.id}`, JSON.stringify({ perpetrator: msg.author.id }), 'EX', 60 * 60);
+      Memer.redis.set(`sand-effect-${msg.author.id}`, JSON.stringify({ perpetrator: msg.author.id }), 'EX', 60 * 60 * 6);
       return `The wind wasn't blowing in your favor and you ended up getting sand in your OWN eyes, good going.\nYou can't steal from ${user.username} for another 6 hours.`;
     } else {
       const sand = await Memer.redis.get(`sand-effect-${user.id}`)
@@ -11,7 +11,7 @@ module.exports = {
       if (sand) {
         return 'This person already has sand in their eyes, no need to bully them further';
       }
-      Memer.redis.set(`sand-effect-${user.id}`, JSON.stringify({ perpetrator: msg.author.id }), 'EX', 60 * 60);
+      Memer.redis.set(`sand-effect-${user.id}`, JSON.stringify({ perpetrator: msg.author.id }), 'EX', 60 * 60 * 6);
       return `You pull out a bit of old, dirty sand from your pocket and blow it right into ${user.username}'s face.\n${user.username} has been blinded and they can't steal from you for another 6 hours.`;
     }
   }

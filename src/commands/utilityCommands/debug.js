7348200fff00036b09d839710b901a0b2c506cc4
Austin Melms <austin@melmsie.com>
2018-10-13 21:45:28 -0400

    wip dont touchy

--- src/commands/utilityCommands/debug.js
+++ src/commands/utilityCommands/debug.js
@@ -49,6 +49,69 @@ module.exports = new GenericCommand(
     const CPUUsage = await getCPUUsage();
     let cached = await Memer.redis.keys('msg-*');
     cached = cached.length;
+    let totalMem = `${(stats.totalRam / 1024).toFixed(2)}/${(os.totalmem() / 1073741824).toFixed(0)}`;
+    let rss = (process.memoryUsage().rss / 1024 / 1024 / 1024).toFixed(2);
+    let heap = (process.memoryUsage().heapUsed / 1024 / 1024 / 1024).toFixed(2);
+    let avgCluster = ((stats.totalRam / 1024) / clusterCount).toFixed(2);
+    return {
+      footer: { text: `Version ${Memer.package.version}` },
+      fields: [
+        {
+          name: 'Guilds',
+          value: `**Total**: ${stats.guilds}\n` +
+          `**Joined**: ${Memer.stats.guildsJoined}\n` +
+          `**Left**: ${Memer.stats.guildsLeft}\n` +
+          `**Large**: ${stats.largeGuilds}\n` +
+          `**Exclusive**: ${stats.exclusiveGuilds}`,
+          inline: true
+        },
+        {
+          name: 'Messages and Users',
+          value: `**Online**: ${stats.users}\n` +
+          `**A.O.U.P.S**: ${(stats.users / stats.guilds).toFixed()}\n` +
+          `**Messages**: ${Memer.stats.messages}\n` +
+          `**Cached**: ${cached}\n` +
+          `**Commands**: ${Memer.stats.commands}`,
+          inline: true
+        },
+        {
+          name: 'Performance',
+          value: `**CPU**: ${CPUUsage.toFixed(0)}%\n` +
+          `**Total Mem**: ${totalMem}gb\n` +
+          `**Process RSS**: ${rss}gb\n` +
+          `**Process Heap**: ${heap}gb\n` +
+          `**Average Cluster**: ${avgCluster}gb`,
+          inline: true
+        },
+        {
+          name: 'Dedi',
+          value: `**Total**: ${stats.guilds}\n` +
+          `**Joined**: ${Memer.stats.guildsJoined}\n` +
+          `**Left**: ${Memer.stats.guildsLeft}\n` +
+          `**Large**: ${stats.largeGuilds}\n` +
+          `**Exclusive**: ${stats.exclusiveGuilds}`,
+          inline: true
+        },
+        {
+          name: 'RethinkDB',
+          value: `**Total**: ${stats.guilds}\n` +
+          `**Joined**: ${Memer.stats.guildsJoined}\n` +
+          `**Left**: ${Memer.stats.guildsLeft}\n` +
+          `**Large**: ${stats.largeGuilds}\n` +
+          `**Exclusive**: ${stats.exclusiveGuilds}`,
+          inline: true
+        },
+        {
+          name: 'Misc',
+          value: `**Total**: ${stats.guilds}\n` +
+          `**Joined**: ${Memer.stats.guildsJoined}\n` +
+          `**Left**: ${Memer.stats.guildsLeft}\n` +
+          `**Large**: ${stats.largeGuilds}\n` +
+          `**Exclusive**: ${stats.exclusiveGuilds}`,
+          inline: true
+        }
+      ]
+    };
 
     const formatted =
     `[GUILDS] ${stats.guilds}\n` +

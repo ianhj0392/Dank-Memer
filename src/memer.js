662b3e995954f2ef4ae3ddedc101d08743b12c64
Austin Melms <austin@melmsie.com>
2018-10-13 20:59:31 -0400

    wrong license boi

--- src/memer.js
+++ src/memer.js
@@ -1,17 +1,3 @@
-/**
- *  Dank Memer: A discord memebot, made to spread dreams of memes, and memes of dreams
- *  Copyright (C) 2018 Dank Memer Team (dankmemerbot@gmail.com)
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published
- *  by the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 const config = require('./config.json');
 const secrets = require('./secrets.json');

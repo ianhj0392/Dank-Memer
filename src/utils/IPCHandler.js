c3abbc7281d7a97b04b833d1fb630ef61a8ce9bc
Paradox <paradoxalcorp@gmail.com>
2018-10-14 18:27:13 +0200

    Add IPC.broadcast(), IPC.broadcastEval() and IPC.register()

--- src/utils/IPCHandler.js
+++ src/utils/IPCHandler.js
@@ -1,5 +1,22 @@
-/** @typedef {import("../models/GenericCommand").Memer} Memer 
+/** @typedef {import('../models/GenericCommand').Memer} Memer
+ * @typedef {import('eris').Guild} Guild
+ * @typedef {import('eris').User} User
+ * @typedef {import('eris').TextChannel} TextChannel
+ * @typedef {import('eris').VoiceChannel} VoiceChannel
+ * @typedef {import('eris').CategoryChannel} CategoryChannel
+ * @typedef {import('eris').AnyGuildChannel} AnyGuildChannel
+ * @typedef {import('eris').Member} Member
+ * @typedef {import('eris').Role} Role
+ * @typedef {import('eris').JSON}
 */
+
+/** @typedef {Object} ConvertedProperties
+ * @prop {Array<Role>} roles An array of roles on this guild
+ * @prop {Array<Member>} members An array of members on this guild
+ * @prop {Array<AnyGuildChannel>} channels An array of channels on this guild
+ */
+
+/** @typedef {Guild & ConvertedProperties} StringifiedGuild */
 
 /** @typedef {Object} ClusterStats
  * @prop {Number} cluster The ID of the cluster
@@ -13,257 +30,305 @@
  */
 
 class IPCHandler {
-    /**
+  /**
      * @param {Memer} client The Memer instance
      */
-    constructor(client) {
-        /** @type {Map} A map of the current ongoing requests */
-        this.requests = new Map();
-        /** @type {Memer} The Memer instance */
-        this.client = client;
-        this._handleIncomingMessage = this._handleIncomingMessage.bind(this);
-        this.client.ipc.register('memerIPC', this._handleIncomingMessage);
-        this._idsGenerated = 0;
-    }
+  constructor (client) {
+    /** @type {Map} A map of the current ongoing requests */
+    this.requests = new Map();
+    /** @type {Memer} The Memer instance */
+    this.client = client;
+    this._handleIncomingMessage = this._handleIncomingMessage.bind(this);
+    this.client.ipc.register('memerIPC', this._handleIncomingMessage);
+    this._idsGenerated = 0;
+  }
 
-    /**
+  /**
      * Returns a new unique ID
      *
      * @readonly
      * @memberof IPCHandler
      * @type {String}
      */
-    get uid () {
-        return `${Date.now()}-${process.pid}-${this._idsGenerated++}`;
-    }
+  get uid () {
+    return `${Date.now()}-${process.pid}-${this._idsGenerated++}`;
+  }
 
-    /**
-     * Fetch the stats of the shards of each cluster
-     * @returns {Promise<Array>} An array of clusters with their shards stats
-     */
-    fetchShardsStats() {
-        const ID = Date.now();
-        return new Promise(resolve => {
-            this.requests.set(ID, {
-                responses: [],
-                resolve: resolve
-            });
-            this.client.ipc.broadcast("fetchShardsStats", {
-                id: ID,
-                type: "fetchShardsStats",
-                clusterID: this.client.clusterID
-            });
-        });
-    }
+  /**
+    *
+    *
+    * @param {String} type The request type
+    * @param {*} data Additional data about the request
+    * @returns {Promise<*>}
+    * @memberof IPCHandler
+    * @private
+    */
+  _createRequest (type, data) {
+    return new Promise((resolve, reject) => {
+      const id = this.uid;
+      this.requests.set(id, {
+        responses: [],
+        resolve,
+        reject
+      });
+      this.client.ipc.broadcast('memerIPC', {
+        type,
+        id,
+        clusterID: this.client.clusterID,
+        data,
+        timeout: setTimeout(() => {
+          const request = this.requests.get(id);
+          if (request) {
+            request.resolve();
+            this.requests.delete(id);
+          }
+        }, 1000 * 15)
+      });
+    });
+  }
 
-    /**
-     *
-     * @param {string} type - The type of the file that should be reloaded, either "command", "event" or "module"
-     * @param {string} path - The absolute path of the file that should be reloaded
-     * @param {string} [name] - If a module, the name of the module
-     * @param {object} [options] - If a module, the options that Reloader.reloadModule() expect
-     * @returns {Promise<array>} An array containing the responses of each clusters, if the reload failed in at least one cluster, the promise is rejected
-     */
-    broadcastReload(type, path, name, options) {
-        const ID = `${this.client.utils.getRandomNumber(1000, 10000) + Date.now()}`;
-        return new Promise((resolve, reject) => {
-            this.requests.set(ID, {
-                responses: [],
-                resolve: resolve,
-                reject: reject
-            });
-            this.client.ipc.broadcast("reload", {
-                id: ID,
-                type: "reload",
-                clusterID: this.client.clusterID,
-                data: {
-                    type: type,
-                    path: path,
-                    name: name,
-                    options: options
-                }
-            });
-        });
-    }
+  _replyTo (message, type, data) {
+    this.client.ipc.sendTo(message.clusterID, 'memerIPC', {
+      type,
+      id: message.id,
+      clusterID: this.client.clusterID,
+      data: data
+    });
+  }
 
-    fetchGuild(id) {
-        if (this.client.bot.guilds.has(id)) {
-            return this.client.bot.guilds.get(id);
-        }
-        const ID = `${this.client.getRandomNumber(1000, 10000) + Date.now()}`;
-        return new Promise((resolve, reject) => {
-            this.requests.set(ID, {
-                responses: [],
-                resolve: resolve,
-                reject: reject
-            });
-            this.client.ipc.broadcast("fetchGuild", {
-                id: ID,
-                type: "fetchGuild",
-                clusterID: this.client.clusterID,
-                data: id
-            });
-        });
+  /**
+    *
+    *
+    * @param {String} id The ID of the guild to fetch
+    * @returns {Promise<StringifiedGuild>}
+    * @memberof IPCHandler
+    */
+  async fetchGuild (id) {
+    if (this.client.bot.guilds.has(id)) {
+      let guild = this.client.bot.guilds.get(id);
+      const members = Array.from(guild.members.values());
+      const roles = Array.from(guild.roles.values());
+      const channels = Array.from(guild.members.values());
+      guild = {
+        ...guild,
+        members,
+        roles,
+        channels
+      };
+      return this.toJSON(guild);
     }
+    return this._createRequest('fetchGuild', id);
+  }
 
-    /**
+  /**
+    *
+    *
+    * @param {String} id The ID of the user to fetch
+    * @returns {Promise<User>}
+    * @memberof IPCHandler
+    */
+  async fetchUser (id) {
+    if (this.client.bot.users.has(id)) {
+      return this.client.bot.users.get(id).toJSON();
+    }
+    return this._createRequest('fetchUser', id);
+  }
+
+  /**
+    *
+    *
+    * @param {String} id The ID of the channel to fetch
+    * @returns {Promise<AnyGuildChannel>}
+    * @memberof IPCHandler
+    */
+  async fetchChannel (id) {
+    let channel;
+    let channelGuild = this.client.bot.guilds.get(this.client.bot.channelGuildMap[id]);
+    if (channelGuild) {
+      channel = channelGuild.channels.get(id);
+    }
+    if (channel) {
+      return this.toJSON(channel);
+    }
+    return this._createRequest('fetchChannel', id);
+  }
+
+  /**
+   * Broadcast the given event to all clusters, uses eris-sharder's IPC
+   *
+   * @param {String} event The name of the event to broadcast
+   * @param {Object?} [data={}] Additional data to broadcast, defaults to `{}`
+   * @returns {void}
+   * @memberof IPCHandler
+   */
+  broadcast (event, data = {}) {
+    this.client.ipc.broadcast(event, data);
+  }
+
+  /**
+   * Register a callback for the given IPC event
+   *
+   * @param {String} event The name of the event to listen to
+   * @param {Function} callback The callback function, a `message` argument may be passed to it, this argument being the additional data emitted along with the event
+   * @returns {void}
+   * @memberof IPCHandler
+   */
+  register (event, callback) {
+    this.client.ipc.register(event, callback);
+  }
+
+  /**
+   *
+   *
+   * @param {String} code
+   * @returns {Promise<Array>} An array containing responses of each cluster
+   * @memberof IPCHandler
+   */
+  async broadcastEval (code) {
+    return this._createRequest('eval', code);
+  }
+
+  /**
      * Called every time the message event is fired on the process
-     * @param {*} message - The message
+     * @param {Object} message - The message
      * @private
      * @returns {void}
      */
-    _handleIncomingMessage(message) {
-        switch (message.type) {
-        case "shardsStats":
-            let request = this.requests.get(message.id);
-            request.responses.push({clusterID: message.clusterID, data: message.data});
-
-            if (this._allClustersAnswered(message.id)) {
-                //Resolve the request and reorder the responses in case it wasn't already
-                request.resolve(request.responses.sort((a, b) => a.clusterID - b.clusterID));
-                this.requests.delete(message.id);
-            }
-            break;
-
-        case "statsUpdate":
-            this.client.stats = message.data;
-            if (!this.clusterCount) {
-                this.clusterCount = message.data.clusters.length;
-            }
-            break;
-
-        case "fetchShardsStats":
-            this.client.ipc.sendTo(message.clusterID, "shardsStats", {
-                type: "shardsStats",
-                id: message.id,
-                clusterID: this.client.clusterID,
-                data: this.client.bot.shards.map(shard => {
-                    return {
-                        id: shard.id,
-                        status: shard.status,
-                        latency: shard.latency,
-                        guilds: this.client.bot.guilds.filter(g => g.shard.id === shard.id).length,
-                        musicConnections: this.client.handlers.MusicManager
-                            ? this.client.handlers.MusicManager.connections.size
-                            : 0
-                    };
-                })
-            });
-            break;
-
-        case "reload":
-            let success = true;
-            try {
-                if (message.data.type === "event") {
-                    this.client.handlers.Reloader.reloadEventListener(message.data.path);
-                } else if (message.data.type === "command") {
-                    this.client.handlers.Reloader.reloadCommand(message.data.path);
-                } else if (message.data.type === "module") {
-                    this.client.handlers.Reloader.reloadModule(message.data.path, message.data.name, message.data.options);
-                } else if (message.data.type === "utils") {
-                    this.client.handlers.Reloader.reloadUtils();
-                } else if (message.data.type === "commands") {
-                    this.client.handlers.Reloader.reloadCommands();
-                } else if (message.data.type === "structures") {
-                    this.client.handlers.Reloader.reloadStructures();
-                } else if (message.data.type === "handlers") {
-                    this.client.handlers.Reloader.reloadHandlers();
-                } else if (message.data.type === "events") {
-                    this.client.handlers.Reloader.reloadEventListener('all');
-                }
-            } catch (err) {
-                success = false;
-                this.client.bot.emit("error", err);
-            }
-
-            this.client.ipc.sendTo(message.clusterID, "reloadDone", {
-                type: "reloadDone",
-                id: message.id,
-                clusterID: this.client.clusterID,
-                data: success
-            });
-            break;
-
-        case "reloadDone":
-            let reloadRequest = this.requests.get(message.id);
-            if (!reloadRequest) {
-                return;
-            }
-            reloadRequest.responses.push({clusterID: message.clusterID, data: message.data});
-
-            if (this._allClustersAnswered(message.id)) {
-                //Resolve the request and reorder the responses in case it wasn't already
-                if (reloadRequest.responses.filter(r => !r.data)[0]) {
-                    reloadRequest.reject(reloadRequest.responses.sort((a, b) => a.clusterID - b.clusterID));
-                } else {
-                    reloadRequest.resolve(reloadRequest.responses.sort((a, b) => a.clusterID - b.clusterID));
-                }
-                this.requests.delete(message.id);
-            }
-            break;
-
-        case "fetchGuild":
-            let guild = this.client.bot.guilds.get(message.data);
-            if (guild) {
-                guild = {
-                    ...guild
-                }; //Shallow clone
-                guild.members = Array.from(guild.members.values());
-                guild.roles = Array.from(guild.roles.values());
-                guild.channels = Array.from(guild.channels.values());
-            }
-            this.client.ipc.sendTo(message.clusterID, "requestedGuild", {
-                type: "requestedGuild",
-                id: message.id,
-                clusterID: this.client.clusterID,
-                data: guild
-            });
-            break;
-
-        case "requestedGuild":
-            if (!this.requests.has(message.id)) {
-                return;
-            }
-            if (this._allClustersAnswered(message.id)) {
-                this.requests.get(message.id).resolve(message.data);
-                // @ts-ignore
-                return this.requests.delete(message.id);
-            }
-            if (message.data) {
-                this.requests.get(message.id).resolve(message.data);
-                // @ts-ignore
-                return this.requests.delete(message.id);
-            }
-            break;
+  _handleIncomingMessage (message) {
+    const request = this.requests.get(message.id);
+    if (!request && message.type.startsWith('requested')) {
+      return;
+    }
+    request.responses.push(message);
+    switch (message.type) {
+      case 'fetchGuild':
+        let guild = this.client.bot.guilds.get(message.data);
+        if (guild) {
+          const members = Array.from(guild.members.values());
+          const roles = Array.from(guild.roles.values());
+          const channels = Array.from(guild.members.values());
+          guild = {
+            ...guild,
+            members,
+            roles,
+            channels
+          };
         }
-        if (process.argv.includes("--dev")) {
-            process.send({
-                name: "log",
-                msg: `Received the message ${message.type} from cluster ${message.clusterID}: ${JSON.stringify(message, null, 2)}`
-            });
+        this._replyTo(message, 'requestedGuild', guild ? guild.toJSON() : undefined);
+        break;
+
+      case 'requestedGuild':
+        if (this._allClustersAnswered(message.id)) {
+          request.resolve(message.data);
+          clearTimeout(request.timeout);
+          return this.requests.delete(message.id);
         }
-    }
+        if (message.data) {
+          this.requests.get(message.id).resolve(message.data);
+          clearTimeout(request.timeout);
+          return this.requests.delete(message.id);
+        }
+        break;
 
-    /**
-     * Check if all the active clusters responded to a request
-     * @param {string} id - The ID of the request to check if all the clusters answered to
-     * @returns {boolean} Whether all the active clusters responded to the request
-     * @private
-     */
-    _allClustersAnswered(id) {
-        return this.requests.get(id).responses.length >= (
-            this.clusterCount
-                ? this.clusterCount - this.client.stats.clusters.filter(c => c.guilds < 1).length
-                : 1)
-            ? true
-            : false;
-    }
+      case 'fetchUser':
+        let user = this.client.bot.users.get(message.data);
+        this._replyTo(message, 'requestedUser', user ? user.toJSON() : user);
+        break;
 
-    _reload() {
-        process.removeListener('message', this._handleIncomingMessage);
-        delete require.cache[module.filename];
-        return new(require(module.filename))(this.client, {requests: this.requests});
+      case 'requestedUser':
+        if (this._allClustersAnswered(message.id)) {
+          request.resolve(message.data);
+          clearTimeout(request.timeout);
+          return this.requests.delete(message.id);
+        }
+        if (message.data) {
+          this.requests.get(message.id).resolve(message.data);
+          clearTimeout(request.timeout);
+          return this.requests.delete(message.id);
+        }
+        break;
+
+      case 'fetchChannel':
+        let channel;
+        let channelGuild = this.client.bot.guilds.get(this.client.bot.channelGuildMap[message.data]);
+        if (channelGuild) {
+          channel = channelGuild.channels.get(message.data);
+        }
+        this._replyTo(message, 'requestedChannel', channel ? channel.toJSON() : channel);
+        break;
+
+      case 'requestedChannel':
+        if (this._allClustersAnswered(message.id)) {
+          request.resolve(message.data);
+          clearTimeout(request.timeout);
+          return this.requests.delete(message.id);
+        }
+        if (message.data) {
+          this.requests.get(message.id).resolve(message.data);
+          clearTimeout(request.timeout);
+          return this.requests.delete(message.id);
+        }
+        break;
     }
+    if (process.argv.includes('--dev')) {
+      this.client.log(`[IPCHandler] - Received the message ${message.type} from cluster ${message.clusterID}: ${JSON.stringify(message, null, 2)}`);
+    }
+  }
+
+  /**
+    * Check if all the active clusters responded to a request
+    * @param {String} id - The ID of the request to check if all the clusters answered to
+    * @returns {Boolean} Whether all the clusters responded to the request
+    * @private
+    */
+  _allClustersAnswered (id) {
+    return this.requests.get(id).responses.length >= this.client.config.sharder.clusters;
+  }
+
+  /**
+   *
+   *
+   * @param {Memer} Memer The Memer instance
+   * @param {String} input The code to evaluate
+   * @returns {Promise<String>} The error message, or `Success`
+   * @memberof IPCHandler
+   */
+  async _eval (Memer, input) {
+    const asynchr = input.includes('return') || input.includes('await');
+    let error;
+    try {
+      await eval(asynchr ? `(async()=>{${input}})();` : input) // eslint-disable-line
+    } catch (err) {
+      error = err.message;
+      Memer.log(err, 'error');
+    }
+    return error || 'Success';
+  }
+
+  /**
+   * Makes the given object stringifiable by JSON.stringify() (converting types if necessary)
+   * @param {Object} object The object to make stringifiable
+   * @returns {Object} The given object, but stringifiable
+   */
+  toJSON (object) {
+    const base = {};
+    for (const key in object) {
+      if (!base.hasOwnProperty(key) && object.hasOwnProperty(key) && !key.startsWith('_')) {
+        if (!object[key]) {
+          base[key] = object[key];
+        } else if (object[key] instanceof Set) {
+          base[key] = Array.from(object[key]);
+        } else if (object[key] instanceof Map) {
+          base[key] = Array.from(object[key].values());
+        } else if (typeof object[key].toJSON === 'function') {
+          base[key] = object[key].toJSON();
+        } else {
+          base[key] = object[key];
+        }
+      }
+    }
+    return base;
+  }
 }
 
 module.exports = IPCHandler;

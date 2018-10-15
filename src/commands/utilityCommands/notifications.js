551c98a85aef2bcee7567ad5c6ed07dcebd97994
Blake <bsward8@gmail.com>
2018-10-16 07:33:37 +1100

    feat: add dismissal support to notifications command

--- src/commands/utilityCommands/notifications.js
+++ src/commands/utilityCommands/notifications.js
@@ -14,13 +14,19 @@ module.exports = new GenericCommand(
         return 'That\'s not a valid notification :rage:';
       }
       return {
+        description: `Grouped under \`${notification.type}\``,
+        color: 10395294,
         fields: [
-          { name: notification.title,
+          { name: `${notification.title}`,
             value: notification.message }
         ],
         timestamp: new Date(notification.timestamp)
       };
     } else if (action === 'dismiss') {
+      let index = value ? isNaN(Number(value) - 1) || !Number.isInteger(Number(value) - 1) ? value : Number(value) - 1 : null;
+      userEntry.dismissNotification(index);
+      await userEntry.save();
+      return `Alright, removed ${value ? (Number.isInteger(index) ? `notification #${index}` : `any notifications grouped under ${index}`) : 'all notifications'}`;
     } else {
       for (let notif in notifications) {
         fields.push(`**${Number(notif) + 1}.** **\`${notifications[notif].title}\`**\n${notifications[notif].message}\n`);
@@ -29,6 +35,7 @@ module.exports = new GenericCommand(
       return Memer.paginationMenu(!fields.length ? ['No notifications, sad :cry:'] : fields, {
         type: 'Notifications',
         embed: {
+          color: 15022389
         },
         pageLength: 5
       }, action);

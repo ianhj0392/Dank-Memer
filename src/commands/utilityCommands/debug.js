26423dc28f1111443c1352643e339d9a33ca139e
Austin Melms <austin@melmsie.com>
2018-10-20 11:45:36 -0400

    revamped debug command

--- src/commands/utilityCommands/debug.js
+++ src/commands/utilityCommands/debug.js
@@ -40,12 +40,6 @@ module.exports = new GenericCommand(
     const stats = await Memer.db.getStats();
     const clusterCount = stats.clusters.length;
 
-    let users = await Memer.r.table('users').count();
-    let guilds = await Memer.r.table('guilds').count();
-    let cd = await Memer.r.table('cooldowns').count();
-    let tags = await Memer.r.table('tags').count();
-    let updates = await Memer.r.table('updates').count();
-    let donors = await Memer.r.table('donors').count();
     const CPUUsage = await getCPUUsage();
     let cached = await Memer.redis.keys('msg-*');
     cached = cached.length;
@@ -82,65 +76,9 @@ module.exports = new GenericCommand(
           `**Process Heap**: ${heap}gb\n` +
           `**Average Cluster**: ${avgCluster}gb`,
           inline: true
-        },
-        {
-          name: 'Dedi',
-          value: `**Total**: ${stats.guilds}\n` +
-          `**Joined**: ${Memer.stats.guildsJoined}\n` +
-          `**Left**: ${Memer.stats.guildsLeft}\n` +
-          `**Large**: ${stats.largeGuilds}\n` +
-          `**Exclusive**: ${stats.exclusiveGuilds}`,
-          inline: true
-        },
-        {
-          name: 'RethinkDB',
-          value: `**Total**: ${stats.guilds}\n` +
-          `**Joined**: ${Memer.stats.guildsJoined}\n` +
-          `**Left**: ${Memer.stats.guildsLeft}\n` +
-          `**Large**: ${stats.largeGuilds}\n` +
-          `**Exclusive**: ${stats.exclusiveGuilds}`,
-          inline: true
-        },
-        {
-          name: 'Misc',
-          value: `**Total**: ${stats.guilds}\n` +
-          `**Joined**: ${Memer.stats.guildsJoined}\n` +
-          `**Left**: ${Memer.stats.guildsLeft}\n` +
-          `**Large**: ${stats.largeGuilds}\n` +
-          `**Exclusive**: ${stats.exclusiveGuilds}`,
-          inline: true
         }
       ]
     };
-
-    const formatted =
-    `[GUILDS] ${stats.guilds}\n` +
-    `  [Joined] ${Memer.stats.guildsJoined}\n` +
-    `  [Left] ${Memer.stats.guildsLeft}\n` +
-    `  [Large] ${stats.largeGuilds}\n` +
-    `  [Exclusive] ${stats.exclusiveGuilds}\n` +
-    `[USERS] ${stats.users}\n` +
-    `  [Average] ${(stats.users / stats.guilds).toFixed()}\n` +
-    `[MESSAGES] ${Memer.stats.messages}\n` +
-    `  [Cached] ${cached}\n` +
-    `[COMMANDS RAN] ${Memer.stats.commands}\n` +
-    `  [Average] ${(Memer.stats.commands / stats.guilds).toFixed(4)}\n` +
-    `[MEMORY] ${(stats.totalRam / 1000).toFixed(1)}/${(os.totalmem() / 1073741824).toFixed(1)}gb (${((stats.totalRam / 1000) / (os.totalmem() / 1073741824)).toFixed(1)}%)\n` +
-    `  [System] ${((os.totalmem() - os.freemem()) / 1073741824).toFixed(1)}/${(os.totalmem() / 1073741824).toFixed(1)}gb (${(((os.totalmem() - os.freemem()) / os.totalmem()) * 100).toFixed(1)}%)\n` +
-    `  [Cluster] ${((stats.totalRam / 1000) / clusterCount).toFixed(1)}gb\n` +
-    `[UPTIME] ${Memer.parseTime(process.uptime())}\n` +
-    `  [System] ${Memer.parseTime(os.uptime())}\n` +
-    `[CPU] ${CPUUsage.toFixed(1)}%\n` +
-    `[ERRORS] ${Memer.stats.errReported}\n` +
-    `  [Eris] ${Memer.stats.err}\n` +
-    `[RETHINK]` +
-    `  [Guilds] ${guilds}\n` +
-    `  [Users] ${users}\n` +
-    `  [Donors] ${donors}\n` +
-    `  [Cooldowns] ${cd}\n` +
-    `  [Tags] ${tags}\n` +
-    `  [Update Channels] ${updates}\n`;
-    return '```ini\n' + formatted + '\n```';
   }, {
     triggers: ['debug'],
     cooldown: 1e4,

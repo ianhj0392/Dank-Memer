28631a88ee0548868a35a48359d243fc0ee3b489
Blake <bsward8@gmail.com>
2018-10-21 16:22:00 +1100

    feat: implement new currency changes into trivia
    also includes other minor refactors to code

--- src/commands/gameCommands/trivia.js
+++ src/commands/gameCommands/trivia.js
@@ -1,16 +1,18 @@
 const GenericCommand = require('../../models/GenericCommand');
 
 module.exports = new GenericCommand(
-  async ({ Memer, msg, addCD }) => {
+  async ({ Memer, msg, addCD, userEntry }) => {
     let data = await Memer.http.get('https://opentdb.com/api.php')
       .query({ amount: 1, type: 'multiple', encode: 'url3986' });
     let trivia = data.body.results[0];
-    let time = 25 * 1000;
+    let time = 10 * 1000;
+    let worth = 0;
 
+    const difficulty = decodeURIComponent(trivia.difficulty);
     let answers = trivia.incorrect_answers;
     answers.push(trivia.correct_answer);
     answers = answers.map(m => { return decodeURIComponent(m); });
-    answers.sort(function (a, b) { // sort the answers in abc order to prevent the correct answer from being in the same spot each time
+    answers.sort((a, b) => { // sort the answers in abc order to prevent the correct answer from being in the same spot each time
       let answerA = a.toLowerCase();
       let answerB = b.toLowerCase();
       if (answerA < answerB) { return -1; }
@@ -19,15 +21,31 @@ module.exports = new GenericCommand(
     });
     let front = answers.map(m => `${answers.indexOf(m) + 1}) *${m}*`).join('\n');
 
+    // Time/reward determination
+    if (difficulty === 'easy') {
+      time = 10 * 1000;
+      worth = 2;
+    } else if (difficulty === 'medium') {
+      time = 15 * 1000;
+      worth = 4;
+    } else { // hard
+      time = 20 * 1000;
+      worth = 8;
+    }
+
     msg.channel.createMessage({ embed: {
-      title: `${msg.author.username}'s trivia question.`,
+      author:
+          {
+            name: `${msg.author.username}'s trivia question`,
+            icon_url: msg.author.dynamicAvatarURL()
+          },
       color: Memer.randomColor(),
-      description: `**${decodeURIComponent(trivia.question)}**\n*Please choose an answer within ${time / 1000}s*\n\n` + front,
+      description: `**${decodeURIComponent(trivia.question)}**\n*You have ${time / 1000} seconds to answer.*\n\n` + front,
       fields: [
         // { name: 'Choices', value: answers.join('\n'), inline: false },
-        // { name: 'Worth', value: `\`${worth} ${worth === 1 ? 'coin' : 'coins'}\``, inline: true },
+        { name: 'Worth', value: `\`${worth} ${worth === 1 ? 'coin' : 'coins'}\``, inline: true },
         // TODO: trivia leaderboards
-        { name: 'Difficulty', value: `\`${decodeURIComponent(trivia.difficulty)}\``, inline: true },
+        { name: 'Difficulty', value: `\`${difficulty.charAt(0).toUpperCase()}${difficulty.slice(1)}\``, inline: true },
         { name: 'Category', value: `\`${decodeURIComponent(trivia.category)}\``, inline: true }
       ],
       footer: { text: 'You can use the number or the word to answer!' }
@@ -37,14 +55,14 @@ module.exports = new GenericCommand(
 
     if (!choice) {
       return 'You did not answer in time, what the heck dude/lady (I do not judge or assume ok)';
-    } else if (choice.content.toLowerCase().includes(decodeURIComponent(trivia.correct_answer).toLowerCase())) {
-      await addCD();
-      return `correct, nice`;
-    } else if (Number(choice.content) === answers.indexOf(decodeURIComponent(trivia.correct_answer)) + 1) {
-      await addCD();
-      return `correct, nice`;
+    }
+
+    await addCD();
+    if (choice.content.toLowerCase().includes(decodeURIComponent(trivia.correct_answer).toLowerCase()) ||
+      Number(choice.content) === answers.indexOf(decodeURIComponent(trivia.correct_answer)) + 1) {
+      await userEntry.addPocket(worth).save();
+      return `correct, nice. You earned **${worth}** coins`;
     } else {
-      await addCD();
       return `no idiot, the correct answer was \`${decodeURIComponent(trivia.correct_answer)}\``;
     }
   },
